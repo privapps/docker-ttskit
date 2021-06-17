0531928ef818b9632d11c28efc50e8430cf9ab18
privapps <privapps@gmx.com>
2021-06-17 14:21:51 -0700

    try action

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM privapps/ttskit:0.1.6_base
 
+WORKDIR /workspace
+
 COPY run.sh /
 COPY run.py /
-RUN mkdir -p $WORKDIR ; chmod a+rwx $WORKDIR run.sh
 
-ENTRYPOINT ["/run.sh"]
+RUN mkdir -p $WORKDIR ; chmod a+rwx $WORKDIR /run.sh
+
+ENTRYPOINT ["python","/run.py"]
--- run.py
+++ run.py
@@ -5,22 +5,22 @@
 # File: /workspace/__voice__.txt is optional with number only, which indicate which voice to use
 # File: /workspace/__out__.wav is the one will be generated
 ##############################################                                                                                                                                                            
-import ttskit, sys
+import ttskit, sys, os
+
+text = os.environ["INPUT_TEXT"]
+voice = os.environ["INPUT_VOICE"]
+wave_path = os.environ["INPUT_CONTENT"]
 
 def getVoice(default_val = 24) -> int:
     try:
-        with open('/workspace/__voice__.txt', 'rt') as f:
-            lines = f.readlines()
-        num = int(''.join(lines))
+        num = int(voice)
         return num if num > 0 and num <= 30 else default_val
         return num
     except Exception:
         return default_val
 
-with open('/workspace/__input__.txt', 'rt') as f:
-    lines = f.readlines()
-wav=ttskit.tts(' '.join(lines),audio=str(getVoice()))
+		
+wav=ttskit.tts(text,audio=str(getVoice()))
 
-file_name='/workspace/__out__.wav'
-with open(file_name, "wb") as f:
+with open(wave_path, "wb") as f:
     f.write(wav)
